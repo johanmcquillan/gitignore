a2aba3d63042ca0680e1b899af6aef27001acfca
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-04-21 11:27:37 +0100

    Script to add header to gitignore file

---
+++ convert.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+for ARG in $@
+do
+    # ARG maybe a code name eg. Python
+    #   or a file name      eg. Python.gitignore
+    case $ARG in
+        *.gitignore) CODE=${ARG%.gitignore} ;;
+        *) CODE=$ARG ;;
+    esac
+    FILE=$CODE.gitignore
+
+    if [ -e $FILE ]
+    then
+        # Header line to add
+        HEADER="### $CODE"
+        # Current top line
+        FILE_HEADER=$(head -n 1 $FILE)
+
+        # If header already present, do nothing
+        if [[ "$HEADER" == "$FILE_HEADER" ]]
+        then
+            echo "$FILE already in correct format"
+        else
+            # Add header and gitignore to temporary file
+            printf "$HEADER\n\n" > tmp
+            cat $FILE >> tmp
+            
+            mv tmp $FILE
+            echo "Converted $FILE"
+        fi
+    else
+        echo "$FILE not found"
+    fi
+done
